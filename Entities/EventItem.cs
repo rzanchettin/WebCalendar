158bfdf92c7b662bc06df46110c9de2e9ebe4bb8
Ricardo Campos Zanchettin <ricardo.zanchettin@hotmail.com>
2026-06-17 18:21:03 -0300

    correções modais de eventos

--- Entities/EventItem.cs
+++ Entities/EventItem.cs
@@ -7,5 +7,7 @@
         public string Name { get; set; } = string.Empty;
         public string Dow { get; set; } = string.Empty; // day of week
         public string MonthAbbrev { get; set; } = string.Empty; // month abbreviation
+        public bool Recurring { get; set; }
+        public int Year { get; set; }
     }
 }
